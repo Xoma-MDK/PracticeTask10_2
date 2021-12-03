de5b82067440dbb129acc802a670246b66b4a075
Daniil Molotkov <molotkov.58@ya.ru>
2021-12-03 10:55:13 +0700

    Добавьте файлы проекта.

---
+++ PracticeTask10_2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PracticeTask10_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите M: ");
+            int M = int.Parse(Console.ReadLine());
+            Console.Write("Введите N: ");
+            int N = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите элементы построчно: ");
+            int[,] array = new int[M, N];
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    array[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            Console.WriteLine($"Введенный массив: ");
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write($"{array[i, j],5}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
